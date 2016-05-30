1032e4320489f8e9972f0f4020e0d962589becdd
reaVen- <marnil37@gmail.com>
2016-05-31 01:56:49 +0200

    testing celery

--- chess_logic/tasks.py
+++ chess_logic/tasks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from celery import shared_task
 
+from chess_logic.views import do_move
 from chess_logic.models import ChessGame
 
 @shared_task
@@ -11,5 +12,6 @@ def test(param):
 @shared_task
 def make_ai_move(game_id):
 	cg = ChessGame.objects.get(pk=game_id)
+
 	return 'turn: %s' % cg.turn
 
