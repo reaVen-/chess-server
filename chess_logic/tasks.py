d0e23414911830f167c879389e44740ab0dcd4d3
reaVen- <marnil37@gmail.com>
2016-05-31 02:34:55 +0200

    testing celery

--- chess_logic/tasks.py
+++ chess_logic/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from celery import shared_task
 from chess_logic.models import ChessGame
+from .chess_rules import move, checkmate, check, pawn_over, replace_pawn
 
 import json, subprocess, time
 
@@ -14,7 +15,99 @@ def make_ai_move(game_id):
     game_data.ab = json.loads(game_data.ab)
     fen = generate_fen(game_data.__dict__)
     best_move = get_best_move(fen).upper()
+    do_move(best_move, game_data)
     return 'FINISHED TASK - BEST MOVE: %s' % best_move
+
+
+def do_move(this_move, cg):
+    ab = json.loads(cg.ab)
+    hb = ab['hb']
+    sb = ab['sb']
+    gameover = cg.game_over
+
+    start = this_move[:2]
+    end = this_move[2:]
+
+
+
+    if cg.turn == "hvit":
+        #if game is not finished
+        if gameover == "0":
+            #figgure out if castling is allowed
+            castling_left, castling_right = False, False
+            if not cg.white_king_moved and not cg.white_tower_left_moved:
+                castling_left = True
+            if not cg.white_king_moved and not cg.white_tower_right_moved:
+                castling_left = True
+
+            #try to move
+            _hb, _sb = move(hb, sb, start, end, hb, sb, castling_left=castling_left, castling_right=castling_right)
+            #check if move was succesful
+            if hb != _hb:
+                #update bricks in database
+                ab = {'hb':_hb, 'sb':_sb}
+                #check if white moved his king or tower
+                if start == "E1" and hb[start] == "Konge":
+                    cg.white_king_moved = True
+
+                #check if white moved his tower from initial position
+                if start == "A1" and hb[start] == "Trn":
+                    cg.white_tower_left_moved = True
+                elif start == "H1" and hb[start] == "Trn":
+                    cg.white_tower_right_moved = True
+
+                #change turn
+                cg.turn = "svart"
+                if pawn_over(_hb):
+                    cg.pawn_over = True
+                    cg.turn = "hvit"
+
+            if checkmate(_sb, _hb, _hb, _sb):
+                if check(_sb, _hb, _hb, _sb):
+                    gameover = "1"
+                    cg.turn = "hvit"
+                else:
+                    gameover = "2"
+                    cg.turn = "hvit"
+
+    elif cg.turn == "svart":
+        if gameover == "0":
+            #figgure out if castling is allowed
+            castling_left, castling_right = False, False
+            if not cg.black_king_moved and not cg.black_tower_left_moved:
+                castling_left = True
+            if not cg.black_king_moved and not cg.black_tower_right_moved:
+                castling_left = True
+
+            _sb, _hb = move(sb, hb, start, end, hb, sb, castling_left=castling_left, castling_right=castling_right)
+            if sb != _sb:
+                ab = {'hb':_hb, 'sb':_sb}
+                #check if black moved his king from initial position
+                if start == "E8" and sb[start] == "Konge":
+                    cg.black_king_moved = True
+
+                #check if black moved his tower from initial position
+                if start == "A8" and sb[start] == "Trn":
+                    cg.black_tower_left_moved = True
+                elif start == "H8" and sb[start] == "Trn":
+                    cg.black_tower_right_moved = True
+
+                cg.turn = "hvit"
+                if pawn_over(_sb):
+                    cg.pawn_over = True
+                    cg.turn = "svart"
+
+            if checkmate(_hb, _sb, _hb, _sb):
+                if check(_hb, _sb, _hb, _sb):
+                    gameover = "1"
+                    cg.turn = "svart"
+                else:
+                    gameover = "2"
+                    cg.turn = "svart"
+    
+    cg.ab = json.dumps(ab)
+    cg.game_over = gameover
+    cg.save()
 
 
 def put(command, engine):
