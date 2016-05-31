17e2ee385c92fd7b4dc61ead78644fb224a0a26d
reaVen- <marnil37@gmail.com>
2016-05-31 02:14:16 +0200

    testing celery

--- chess_logic/tasks.py
+++ chess_logic/tasks.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
-
 from celery import shared_task
-
 from chess_logic.models import ChessGame
+
+import json, subprocess, time
 
 @shared_task
 def test(param):
