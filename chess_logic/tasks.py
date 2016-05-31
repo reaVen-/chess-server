5eceee1217370b14c22eedf8060121f82a36ca06
reaVen- <marnil37@gmail.com>
2016-05-31 02:42:44 +0200

    testing celery

--- chess_logic/tasks.py
+++ chess_logic/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from celery import shared_task
 from chess_logic.models import ChessGame
-from .chess_rules import move, checkmate, check, pawn_over, replace_pawn
+from .chess_rules import move, checkmate, check, pawn_over, replace_pawn, brick_to_index
 
 import json, subprocess, time
 
