e504c39700b538bfb03feda79f5c19c1448616d8
reaVen- <marnil37@gmail.com>
2016-06-15 17:39:32 +0200

    added more requirements

--- example_configs/local_settings.py
+++ example_configs/local_settings.py
@@ -10,3 +10,17 @@ DATABASES = {
         'PORT' : '5432',
             }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = '/apps/chess-server/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/apps/chess-server/media'
+
+#celery
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout':60}
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
