577981c0df68b97cc06f11e84528d1660cbdf7d0
reaVen- <marnil37@gmail.com>
2016-06-16 20:16:04 +0200

    testin channels

--- chess_logic/static/js/push.js
+++ chess_logic/static/js/push.js
@@ -1,8 +1,7 @@
 $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/id" + window.location.pathname);
-    console.log(game_id);
+    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/id/" + game_id);
 
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
