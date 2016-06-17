72cc2d6619a9449c495f9f6b92005df449400a2e
reaVen- <marnil37@gmail.com>
2016-06-17 17:57:33 +0200

    testin channels

--- chess_logic/consumers.py
+++ chess_logic/consumers.py
@@ -19,51 +19,9 @@ def ws_connect(message):
 
 @channel_session
 def ws_receive(message):
-    # Look up the room from the channel session, bailing if it doesn't exist
-    print "RECEIVED MESSAGE"
-
-    data = message['text']
-    print data
-
+    #is not used at the moment
     Group("id-"+message.channel_session['game_id']).send({'text':json.dumps({'foo':'bar'})})
-
-    """
-    try:
-        label = message.channel_session['room']
-        room = Room.objects.get(label=label)
-    except KeyError:
-        log.debug('no room in channel_session')
-        return
-    except Room.DoesNotExist:
-        log.debug('recieved message, buy room does not exist label=%s', label)
-        return
-
-    # Parse out a chat message from the content text, bailing if it doesn't
-    # conform to the expected message format.
-    try:
-        data = json.loads(message['text'])
-    except ValueError:
-        log.debug("ws message isn't json text=%s", text)
-        return
-    
-    if set(data.keys()) != set(('handle', 'message')):
-        log.debug("ws message unexpected format data=%s", data)
-        return
-
-    if data:
-        log.debug('chat message room=%s handle=%s message=%s', 
-            room.label, data['handle'], data['message'])
-        m = room.messages.create(**data)
-
-        # See above for the note about Group
-        Group('chat-'+label, channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())})
-    """
 
 @channel_session
 def ws_disconnect(message):
-    try:
-        label = message.channel_session['room']
-        room = Room.objects.get(label=label)
-        Group('chat-'+label, channel_layer=message.channel_layer).discard(message.reply_channel)
-    except (KeyError, Room.DoesNotExist):
-        pass
+    pass
