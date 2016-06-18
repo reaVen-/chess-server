5e016c6c04ea74bb04914873752a8b7455074bfa
reaVen- <marnil37@gmail.com>
2016-06-18 11:38:20 +0200

    implementing channels on homepage to receive realtime updates of active matches

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, render
 from django.template import RequestContext
 from home.models import ChessUser, Challenge
 from django import forms
