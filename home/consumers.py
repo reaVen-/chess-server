57bafb310730e73dab7c39c711f05d93dd31ca92
reaVen- <marnil37@gmail.com>
2016-06-18 10:57:35 +0200

    implementing channels on homepage to receive realtime updates of active matches

--- home/consumers.py
+++ home/consumers.py
@@ -3,11 +3,11 @@ from channels import Group
 from channels.sessions import channel_session
 
 def home_connect(message):
-	prefix, label = message['path'].decode('ascii').strip("/").split("/")
+    prefix, label = message['path'].decode('ascii').strip("/").split("/")
 
-    print "prefix: %s, label: %s"%(prefix, label)
+    print "home -- prefix: %s, label: %s"%(prefix, label)
 
     Group("user-%s" % label, channel_layer=message.channel_layer).add(message.reply_channel)
 
 def home_disconnect(message):
-	print "home disconnect"
+    print "home disconnect"
